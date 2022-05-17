b3e4482d9d3c615efbdbff1b0c73e891a58d2109
Joshua Thieme <jomthieme@gmail.com>
2022-05-17 14:15:40 -0600

    Added some new clases

---
+++ app/src/main/java/com/example/flashcardapp/Game.kt
@@ -0,0 +1,14 @@
+package com.example.flashcardapp
+
+class Game {
+    val practiceNum = 0
+    val type : MathType
+        get() {
+            TODO()
+        }
+    val generatedNum = 0
+    val answer = 0
+
+
+
+}
---
+++ app/src/main/java/com/example/flashcardapp/MathType.kt
@@ -0,0 +1,11 @@
+package com.example.flashcardapp
+
+enum class MathType(val type : String) {
+
+    // Initalize the enumeration types
+    ADD("+"),
+    SUBTRACT("-"),
+    MULTIPLY("X"),
+    DIVIDE("/")
+
+}
